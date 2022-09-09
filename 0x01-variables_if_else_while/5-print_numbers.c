15a913905fe2b5f3f22fbcb8252e978489de3c8f
Kendralene <ehikendra93@gmail.com>
2022-09-09 14:42:57 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+int e;
+e = 0;
+while (e < 10)
+{
+printf("%d", e++);
+}
+printf("\n");
+return (0);
+}
