c1ed73a9997d017fb774050e9d2667d1c5eba1a4
Kendralene <ehikendra93@gmail.com>
2022-09-10 00:27:48 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+char e;
+for (e = 'z'; e >= 'a'; e--)
+{
+putchar(e);
+}
+putchar('\n');
+return (0);
+}
