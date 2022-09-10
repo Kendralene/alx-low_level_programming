10ae09af62de1c695e01bcefbe4280bf849ab815
Kendralene <ehikendra93@gmail.com>
2022-09-10 00:30:30 -0700

    Hexedecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+int e;
+char p;
+for (e = 48; e <= 57; e++)
+{
+putchar(e);
+}
+for (p = 'a'; p <= 'f'; p++)
+{
+putchar(p);
+}
+putchar('\n');
+return (0);
+}
