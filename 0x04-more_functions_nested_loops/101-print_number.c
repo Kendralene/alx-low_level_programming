3248242f0e79615da06c75b0080f00f34c094fa8
Kendralene <ehikendra93@gmail.com>
2022-09-20 03:56:01 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,29 +13,29 @@ unsigned int i, j, count;
 if (n < 0)
 {
 
-_putchar(45);
-i = n * -1;
+     _putchar(45);
+       i = n * -1;
 }
 
 else
 {
-i = n;
+        i = n;
 }
 
-j = i;
-count = 1;
+         j = i;
+         count = 1;
 
 while (j > 9)
 {
 
-j /= 10;
-count *= 10;
+         j /= 10;
+         count *= 10;
 }
 
 for (; count >= 1; count /= 10)
 {
 
-_putchar(((i / count) % 10) + 48);
+         _putchar(((i / count) % 10) + 48);
 }
 
 }
