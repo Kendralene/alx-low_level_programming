b7c42ea63ee4e2a8e668ca254af6059ca9a41555
Kendralene <ehikendra93@gmail.com>
2022-09-20 21:29:02 -0700

    Advance task 2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,28 +3,29 @@
 #include <time.h>
 
 /**
- *main - generates random valid passwords
- *Return: 0 (on success).
+ * main - generates random valid passwords 
+ *
+ * Return: Always 0
  */
 
 int main(void)
 {
 
-int pass, sum;
+int myrand;
+
+int count;
+
+int total;
 
 srand(time(NULL));
-sum = 0;
-
-while (sum <= 2645)
+for (count = 0, total = 2772; total > 122; count++)
 {
+myrand = (rand() % 125) + 1;
 
-pass = (rand() % 128);
-sum += pass;
-
-printf("%c", pass);
+printf("%c", myrand);
+total -= myrand;
 }
-
-printf("%c", 2772 - sum);
+printf("%c", total);
 
 return (0);
 }
