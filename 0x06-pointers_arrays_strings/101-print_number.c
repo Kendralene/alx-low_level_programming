adff279c144fc74d7ce396ef72383351ac834a0d
Kendralene <ehikendra93@gmail.com>
2022-09-23 17:09:19 -0700

    task 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,12 +3,12 @@
 /**
  * print_number - prints an integer
  *
- * @num: int to print  
+ * @num: int to print
  *
  * Return: void
  */
 
-void print_number(int num)
+void print_number(int n)
 {
 unsigned int d = 10, n;
 
@@ -18,14 +18,12 @@ _putchar('-');
 num *= -1;
 }
 n = num;
-
 if (n < d)
 {
 _putchar('0' + n);
 }
 else
 {
-
 while (n >= d)
 {
 d *= 10;
@@ -33,11 +31,10 @@ if (d >= 1000000000)
 if (d == 1000000000)
 break;
 }
-
 if (!(d >= 1000000000) || n > 100000000)
-if (!(d == 1000000000) || n == 123456789)d /= 10;
+if (!(d == 1000000000) || n == 123456789)
+d /= 10;
 _putchar('0' + n / d);
-
 while (d != 10)
 {
 d /= 10;
